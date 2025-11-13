c55c6696a9b85bd1d1b1322b1eeff41bbb6d2fe6
TakumiMukaiyama <takumi1012tyanmuja@gmail.com>
2025-11-13 23:45:13 +0900

    update

--- week78-session-variables/solution.sql
+++ week78-session-variables/solution.sql
@@ -1,8 +1,24 @@
-SET sales_avg = (SELECT AVG(sales_amount) FROM w78);
+CREATE TABLE w78 AS
+SELECT
+  SEQ4() AS sales_id,
+  CASE
+    WHEN MOD(SEQ4(), 4) = 0 THEN 'Product A'
+    WHEN MOD(SEQ4(), 4) = 1 THEN 'Product B'
+    WHEN MOD(SEQ4(), 4) = 2 THEN 'Product C'
+    ELSE 'Product D'
+  END AS product_name,
+  UNIFORM(1, 10, RANDOM())::INTEGER AS quantity_sold,
+  DATEADD('day', -UNIFORM(1, 365, RANDOM())::INTEGER, CURRENT_DATE()) AS sales_date,
+  UNIFORM(20, 100, RANDOM())::FLOAT * UNIFORM(1, 10, RANDOM())::INTEGER AS sales_amount
+FROM TABLE(GENERATOR(ROWCOUNT => 1000));
+-- ここまで実行済み
+
+SET sales_avg = (SELECT AVG(sales_amount) from w78);
 
 SELECT *
 FROM w78
 WHERE sales_amount between $sales_avg - 50 and $sales_avg +50;
+
 
 -- Set multiple session variables for dynamic analysis
 SET sales_avg = (SELECT AVG(sales_amount) FROM w78);
